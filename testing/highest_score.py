60ca689aa8b334acc2ee64f9b44a6d3079418932
Skuld <42847091+skuldyami@users.noreply.github.com>
2019-04-03 18:10:40 -0300

    fixed highest_score.py so it uses split()

--- testing/highest_score.py
+++ testing/highest_score.py
@@ -1,7 +1,10 @@
+name=""
+score="0"
 highest_score=0
 results_f=open("results.txt")
 
 for line in results_f:
-	if float(line)>highest_score:
-		highest_score=float(line)
+  (name, score)=line.split()      #multiple assignment
+  if float(score)>highest_score:
+    highest_score=float(score)
 print("The higher score was: "+str(highest_score))
