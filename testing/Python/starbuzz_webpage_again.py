9fcb23dcd619c4111ecf824fb285eb000b170249
Skuld <42847091+skuldyami@users.noreply.github.com>
2020-03-16 16:40:54 -0400

    time in python

--- testing/Python/starbuzz_webpage_again.py
+++ testing/Python/starbuzz_webpage_again.py
@@ -1,6 +1,13 @@
 import urllib.request
+import time
 
-page= urllib.request.urlopen("file:///F:/Claudy/Study/CS/Developing/praxis/testing/Python/prices.html")
-text= page.read().decode("utf8")
-price= text[219:223]
-print(price)
+price= 99.99
+while price > 4.74:
+  time.sleep(15)
+  page= urllib.request.urlopen("file:///F:/Claudy/Study/CS/Developing/praxis/testing/Python/prices-royalty.html")
+  text= page.read().decode("utf8")
+  search_str= ">$"
+  str_length= 4;
+  pos= text.find(search_str)+len(search_str)
+  price=float(text[pos:pos+str_length])
+print("price= "+str(price)+" Buy!!!")
