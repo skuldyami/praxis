125558b7b8d3909c57453acbabb658b01658f6e4
Skuld <42847091+skuldyami@users.noreply.github.com>
2020-04-04 08:52:40 -0300

    better JS lol

--- testing/JS/chessboard.js
+++ testing/JS/chessboard.js
@@ -1,11 +1,9 @@
 let strChar='#';
 let res='';
 let size=8;
-let count=0;
 for(let i=0; i<size; i++){
   for(let j=0; j<size; j++){
     res=(i%2==0)?((j%2==0)?res+=' ':res+=strChar):(j%2==0)?res+=strChar:res+=' ';
-    count++;
   }
   console.log(res);
   res='';
